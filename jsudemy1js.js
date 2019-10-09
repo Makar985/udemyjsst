83211d4c044f79b71055ac1b6423172e20fb3257
Makar <52992011+Makar985@users.noreply.github.com>
2019-10-09 15:03:30 +0300

    Update jsudemy1js.js
    Operators video

--- jsudemy1js.js
+++ jsudemy1js.js
@@ -25,14 +25,64 @@ var johnMark = 'John and Mark'
 */
 
 /*
- Variable mutation and type correction
+ Variable mutation and type coercion
  */
 
+//Type coercion
+/*
 var firstName = 'John';
 var age = 28;
 
 console.log(firstName + ' ' + age);
 
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is ' + age + ' years old ' +
+ job + '. Is he married ?' + isMarried);
+*/
+
+//Variable mutation
+
+/*
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is ' + age + ' years old ' +
+            job + '. Is he married ?' + isMarried)
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
+*/
+
+/***********
+ * Basic operators
+*/
+var now, yearJohn, yearMark;
+now = 2018;
+ageJohn = 28;
+ageMark = 33;
+
+//Math operators
+
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now * 2);
+console.log(now / 2);
+
+//Loical operators
+var johnOlder = ageJohn < ageMark;
+console.log(johnOlder);
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark');
+var x;
+console.log(typeof x);
 
 
 
