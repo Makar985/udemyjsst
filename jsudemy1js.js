3dc5366f6a2db2af934705cc1228e8a0507f7b82
Makar <52992011+Makar985@users.noreply.github.com>
2019-10-09 09:09:45 +0300

    Update jsudemy1js.js
    Next video check out

--- jsudemy1js.js
+++ jsudemy1js.js
@@ -1,1 +1,46 @@
-console.log('Hello World!!!');
+/*
+Variables and datatypes
+*/
+
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge)
+
+var job;
+console.log(job)
+
+job = 'Teacher'
+console.log(job)
+ 
+// varable naming rules
+
+var _3years = 3;
+var johnMark = 'John and Mark'
+*/
+
+/*
+ Variable mutation and type correction
+ */
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+
+
+
+
+
+
+
+
+
+
+
